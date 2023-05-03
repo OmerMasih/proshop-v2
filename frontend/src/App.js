fffe8551d540baa9ab5e1644ceb0afb55ed82d4b
OmerMasih <you@example.com>
2023-05-02 20:23:26 -0700

    Created my footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div>Welcome to proshop</div>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <container>
+          <h1>Welcome to proShop</h1>
+        </container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
