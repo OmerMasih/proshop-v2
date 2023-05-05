5b4d99a781bc1a1753db9bdc13b4a9c977809d10
OmerMasih <you@example.com>
2023-05-04 17:43:34 -0700

    added information

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,16 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
     <>
       <Header />
       <main className="py-3">
-        <container>
-          <h1>Welcome to proShop</h1>
-        </container>
+        <Container>
+          <Outlet />
+        </Container>
       </main>
       <Footer />
     </>
